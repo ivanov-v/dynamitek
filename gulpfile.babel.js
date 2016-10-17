250769dd6960724f827944cda502482cdee2b695
Vadim Ivanov <ivanov@morizo.ru>
2016-10-17 12:41:00 +0300

    Добавлен таск build

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,17 @@
 
 import gulp from 'gulp';
 import requireDir from 'require-dir';
+import run from 'run-sequence';
 
 requireDir('./gulp/tasks');
 
-gulp.task('watch', () => {
-
+gulp.task('build', (fn) => {
+  run(
+    'clean',
+    'styles',
+    'copy',
+    'images',
+    'symbols',
+    fn
+  );
 });
