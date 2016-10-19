3338b24997f0d94f6a008f71cda9cf426a662b47
Vadim Ivanov <ivanov@morizo.ru>
2016-10-19 11:10:52 +0300

    Добавлены svg в отслеживаемые файлы

--- gulp/paths.js
+++ gulp/paths.js
@@ -1,22 +1,22 @@
 export default {
   baseSrc: './src',
-	baseBuild: './build',
+  baseBuild: './build',
 
-	src: {
-		styles: './src/styles/app.scss',
-		scripts: './src/scripts/**/*.js',
+  src: {
+    styles: './src/styles/app.scss',
+    scripts: './src/scripts/**/*.js',
     fonts: './src/fonts/**/*.{woff,woff2}',
-    images: './src/img/**/*.{png,jpg,gif}',
-    media: './src/media/**/*.{png,jpg,gif}',
+    images: './src/img/**/*.{png,jpg,gif,svg}',
+    media: './src/media/**/*.{png,jpg,gif,svg}',
     symbols: './src/sprite/symbols/*.svg',
     views: './src/views/*.pug'
-	},
+  },
 
-	build: {
-		styles: './build/css',
-		images: './build/img',
-		media: './build/media',
-		scripts: './build/js',
+  build: {
+    styles: './build/css',
+    images: './build/img',
+    media: './build/media',
+    scripts: './build/js',
     views: './build'
-	}
-}
+  }
+};
